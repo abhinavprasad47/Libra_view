30b822e09126a5d8b99574198ce9767999614d36
abhinav <abhinavprasad791@gmail.com>
2019-08-18 22:33:05 +0530

    components

---
+++ src/Menu.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <aside className="menu">
+      <ul className="menu-list">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>All Logs</a>
+        </li>
+        <li>
+          <a>All books</a>
+        </li>
+        <li>
+          <a>All authors</a>
+        </li>
+        <li>
+          <a>Add user</a>
+        </li>
+        <li>
+          <a>Log Book Echanges</a>
+        </li>
+        <li>
+          <a>Add Books</a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Menu;
