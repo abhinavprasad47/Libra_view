b7f2eb538867c9a72c0b553691afa84d1b0691c1
abhinav <abhinavprasad791@gmail.com>
2019-08-18 23:36:31 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import "bulma/css/bulma.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Menu from "./Menu.js";
 import Nav from "./Nav.js";
+import Home from "./components/Home.js";
+
+
 
 function App() {
- return(
-   <div>
-   <Nav />
-   <Menu />
-   </div>
-   
+  return (
+    <div className="App">
+      <Nav />
+      <Menu />
+
+    </div>
   );
 }
 
