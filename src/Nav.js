aa9e56a4c11255230869e005f56e76c5899b7f1f
abhinav <abhinavprasad791@gmail.com>
2019-08-18 22:33:20 +0530

    components

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Nav() {
+
+return (
+
+    <div className = "nav-bar">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+            <img src="cek.png" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="50" height="75"/>
+        </a>
+    </div>
+        </nav>
+    </div>
+    );
+    };
+
+export default Nav;
