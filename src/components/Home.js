da878f52fbb08e5e7e64879177e1d951968e0638
abhinav prasad <abhinavprasad791@gmail.com>
2019-08-19 18:20:03 +0530

    home.js rename

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div ClassName = "home">
+        <p>Library Management System</p>
+        </div>
+    );
+}
+
+export default Home;
