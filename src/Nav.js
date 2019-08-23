2f520685dd4c27cf7182009922c1d84d960bd73b
abhinav <abhinavprasad791@gmail.com>
2019-08-23 10:16:02 +0530

    -

--- src/Nav.js
+++ src/Nav.js
@@ -8,7 +8,7 @@ return (
     <div className = "nav-bar">
       <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-        <a className="navbar-item" href="/home.html">
+        <a className="navbar-item" href="/index.html">
             <img src="https://avatars2.githubusercontent.com/u/40546547?s=400&u=851a17df61f900d7ac4e1146e7e6309afdca4226&v=4" alt="CEK OPEN SOURCE CLUB"/>
         </a>
     </div>
