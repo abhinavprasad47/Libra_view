dcdcf645e6e2545b1fa456a60477fa53aaa94b2c
abhinav <abhinavprasad791@gmail.com>
2019-08-23 10:12:49 +0530

    logo set

--- src/Nav.js
+++ src/Nav.js
@@ -8,8 +8,8 @@ return (
     <div className = "nav-bar">
       <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-            <img src="https://avatars2.githubusercontent.com/u/40546547?s=400&u=851a17df61f900d7ac4e1146e7e6309afdca4226&v=4" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="50" height="75"/>
+        <a className="navbar-item" href="/home.html">
+            <img src="https://avatars2.githubusercontent.com/u/40546547?s=400&u=851a17df61f900d7ac4e1146e7e6309afdca4226&v=4" alt="CEK OPEN SOURCE CLUB"/>
         </a>
     </div>
         </nav>
