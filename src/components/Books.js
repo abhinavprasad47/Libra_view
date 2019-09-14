0aafd48d662474628c05d0cb4f9100a4dbf71338
abhinavprasad47 <abhinavprasad791@gmail.com>
2019-09-15 05:16:03 +0530

    functional to class

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React ,{ Component} from "react";
 
-function Books () {
+class Books extends Component {
+    render(){
     return(
         <div>
             <h1>ddhdgdfgd</h1>
         </div>
     );
-};
+}
+}
 
 export default Books ;
