a3a3c5ae9f42424c3e7ab3f7282b6db38a930147
abhinavprasad47 <abhinavprasad791@gmail.com>
2019-09-15 05:15:58 +0530

    functional to class

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React ,{ Component} from "react";
 
-function Author () {
+
+class Author extends Component {
+    render() {
     return(
         <div>
             <h1>sfsfs</h1>
         </div>
-    );
-};
+    )
+    }
+}
 
-export default Author ;
+export default Author;
