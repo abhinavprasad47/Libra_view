d0b0065ad8255d8a78cbe0a5454d46131538e49d
abhinavprasad47 <abhinavprasad791@gmail.com>
2019-09-15 05:16:13 +0530

    functional to class

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React ,{Component} from "react";
 
-function Logs () {
+class Logs extends Component{
+    render(){
     return(
         <div>
             <h1>dadad</h1>
         </div>
     );
-};
+    }
+}
 
 export default Logs ;
