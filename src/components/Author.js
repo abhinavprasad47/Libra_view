7bd5b89861ec80211ff12638879fd07f75a2cdf0
abhinavprasad47 <abhinavprasad791@gmail.com>
2019-09-15 10:33:02 +0530

    api-test

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,14 +1,58 @@
 import React ,{ Component} from "react";
 
+import Table from 'react-bootstrap/Table'
+
+import axios from 'axios';
+
 
 class Author extends Component {
+    constructor(){
+        super()
+        this.state = {
+            author : []
+        }
+    }
+    componentDidMount() {
+        axios.get(`http://libra-cek.herokuapp.com/API/Authors/`,{
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json',
+            },
+            withCredentials: true,
+            credentials: 'same-origin',
+        })
+      .then(res => {
+        const author = res.data;
+        this.setState({ author });
+      })
+      } 
     render() {
     return(
         <div>
-            <h1>sfsfs</h1>
-        </div>
+            <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+    </tr>
+  </thead>
+  {this.state.author.map(author => (
+  <tbody>
+    <tr>
+      <td>{author.id}</td>
+      <td>{author.first_name}</td>
+      <td>{author.last_name}</td>
+    </tr>
+  </tbody>
+  ))
+  }
+</Table>
+</div>
     )
-    }
+}
 }
 
 export default Author;
